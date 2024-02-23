51fc9a5ffba2750ed2cf99686b7d3c7ee5114d4a
Mike Tilburg <mtilburg@adobe.com>
2024-02-23 15:33:02 +0100

    test

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,35 +1,16 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './aem.js';
+import { sampleRUM, loadScript } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
-async function loadScript(src, parent, attrs) {
-  return new Promise((resolve, reject) => {
-    if (!document.querySelector(`${parent} > script[src="${src}"]`)) {
-      const script = document.createElement('script');
-      script.src = src;
-      if (attrs) {
-        // eslint-disable-next-line no-restricted-syntax, guard-for-in
-        for (const attr in attrs) {
-          script.setAttribute(attr, attrs[attr]);
-        }
-      }
-      script.onload = resolve;
-      script.onerror = reject;
-      document.head.append(script);
-    } else {
-      resolve();
-    }
-  });
-}
 
 // AGS
 //await loadScript('https://assets.adobedtm.com/6a74768abd57/ae0061acbe82/launch-3ee9ff2398b5-development.min.js', 'head', { async: true });
 
 // Summit
-await loadScript('https://assets.adobedtm.com/78af697641c3/8c86e24f92f7/launch-ab63899eb224-development.min.js', 'head', { async: true });
+await loadScript('https://assets.adobedtm.com/78af697641c3/8c86e24f92f7/launch-159c1d47c2de.min.js'/*, 'head'*/, { async: true });
 
 /*
 // Load Launch properties (adobedtm)
